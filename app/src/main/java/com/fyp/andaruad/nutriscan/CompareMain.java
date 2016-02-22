0b2bf6d2d3f3c9b687379febe1192bb79abb9dc9
Andaru Adiwignya <andaruad@gmail.com>
2016-02-22 21:07:41 +0800

    Added compare class;

---
+++ app/src/main/java/com/fyp/andaruad/nutriscan/CompareMain.java
@@ -0,0 +1,20 @@
+package com.fyp.andaruad.nutriscan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Andaruad on 22/02/2016.
+ */
+public class CompareMain extends Activity {
+
+    @Override
+    public void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result_main);
+
+    }
+
+
+
+}
