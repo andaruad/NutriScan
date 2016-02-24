fe64f6a527a4f0bda7e1cf3027a93e185fb0032b
Andaru Adiwignya <andaruad@gmail.com>
2016-02-24 19:30:14 +0800

    Move Main; added img background

--- app/src/main/java/com/fyp/andaruad/nutriscan/ProductsDB.java
+++ app/src/main/java/com/fyp/andaruad/nutriscan/ProductsDB.java
@@ -1,18 +1,34 @@
 package com.fyp.andaruad.nutriscan;
 
-import android.app.Activity;
-
 /**
  * Created by Andaruad on 22/02/2016.
  */
-public class ProductsDB extends Activity {
+public class ProductsDB  {
 
+    public String p_name;
+    public int p_calories;
 
+public ProductsDB(String p_name, int p_calories){
 
+    this.p_name = p_name;
+    this.p_calories = p_calories;
+}
 
+public String getp_name(){
+    return p_name;
+}
 
+public int getp_calories(){
+    return p_calories;
+}
 
+public void setp_name(String p_name) {
+    this.p_name = p_name;
+}
 
+public void setp_calories (int p_calories){
+    this.p_calories = p_calories;
+}
 
-
+//
 }
