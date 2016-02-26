6b7164af8d59a81ee06c23b5aa15cf9148ef8397
Andaru Adiwignya <andaruad@gmail.com>
2016-02-26 15:14:53 +0800

    Minor Changes;

--- app/src/main/java/com/fyp/andaruad/nutriscan/MainActivity.java
+++ app/src/main/java/com/fyp/andaruad/nutriscan/MainActivity.java
@@ -89,7 +89,7 @@ public class MainActivity extends AppCompatActivity
                 String re = scanResult.getContents();
                 Log.d("code", re);
                      TextView tvresult = (TextView) findViewById(R.id.tvresult);
-                    tvresult.setText("Your scanned barcode is" + re);
+                    tvresult.setText("Your scanned barcode is: " + re);
 
 
 
