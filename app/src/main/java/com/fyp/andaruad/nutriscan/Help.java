fc85f48e775e311f6f58f384ca39130425abf747
Andaru Adiwignya <andaruad@gmail.com>
2016-02-26 15:27:26 +0800

    Minor Changes;

---
+++ app/src/main/java/com/fyp/andaruad/nutriscan/Help.java
@@ -0,0 +1,16 @@
+package com.fyp.andaruad.nutriscan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Andaruad on 26/02/2016.
+ */
+public class Help extends Activity {
+
+
+    @Override
+    public void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);}
+}
