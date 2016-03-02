60646a0f19daaf8f1ca09430a939da9042353ecb
Andaru Adiwignya <andaruad@gmail.com>
2016-03-02 12:56:00 +0800

    Imported icon images; Added CompareMain function; Changing button to Image button layout;

--- app/src/main/java/com/fyp/andaruad/nutriscan/CompareMain.java
+++ app/src/main/java/com/fyp/andaruad/nutriscan/CompareMain.java
@@ -2,6 +2,11 @@ package com.fyp.andaruad.nutriscan;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.google.zxing.integration.android.IntentIntegrator;
 
 
 //* Created by Andaruad on 22/02/2016.
@@ -12,7 +17,31 @@ public class CompareMain extends Activity {
     public void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.compare_main);
+
+        ImageButton rescan = (ImageButton) findViewById(R.id.brescan);
+        rescan.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator integrator = new IntentIntegrator(CompareMain.this);
+                integrator.initiateScan();
+            }});
+
+        ImageButton compare = (ImageButton) findViewById(R.id.bcompare2);
+        compare.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator integrator = new IntentIntegrator(CompareMain.this);
+                integrator.initiateScan();
+            }});
+
+
+
+
     }
+
+
+
+
 
 
 
