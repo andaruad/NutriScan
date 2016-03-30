bc5f784c8c23c68ea301ca1c83765b3d2a0b580e
Andaru Adiwignya <andaruad@gmail.com>
2016-03-30 17:59:49 +0800

    Added calories to serving in Result Activity; Updated img_result; Minor appearance tweak on result layout;

--- app/src/main/java/com/fyp/andaruad/nutriscan/ResultActivity.java
+++ app/src/main/java/com/fyp/andaruad/nutriscan/ResultActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ImageButton;
 import android.widget.TextView;
-
+import java.math.*;
 /*
  * Created by Andaruad on 16/02/2016.
  */
@@ -16,6 +16,7 @@ public class ResultActivity extends Activity {
         setContentView(R.layout.result_main);
         TextView pname = (TextView)findViewById(R.id.pname);
         TextView cal = (TextView)findViewById(R.id.calories);
+        TextView cal2 = (TextView)findViewById(R.id.calories2);
         DBHandler dbHandler = new DBHandler(this);
         long productid  = getIntent().getLongExtra("productid", 0);
         Product product = dbHandler.Get_Product(productid);
@@ -24,11 +25,12 @@ public class ResultActivity extends Activity {
         if(product != null){
             pname.setText(product.getName());
             cal.setText(product.getCal());
+            int cals = (Integer.parseInt(product.getCal().toString())/2);
+            cal2.setText(cals+"");
         } else {
             pname.setText("Product not found");
         }
     }
-
 
 
 
